7bee5d855d7fb1c83a3954e9fa9e75006ee9f4cd
Anrey <mr.baltsevich@mail.ru>
2020-11-11 17:26:23 +0300

    Commit for Task

--- src/index.js
+++ src/index.js
@@ -15,3 +15,8 @@ ReactDOM.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+ReactDOM.render(
+<h1>Hello World!</h1>,
+document.getElementById('root')
+	);
